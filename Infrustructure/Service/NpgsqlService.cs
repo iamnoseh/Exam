e10d091b15d9673ddc115370a5afa74d6a6fb5ff
Noseh Taghaymurodzoda <noseh.tagaymurodzoda@gmail.com>
2024-12-04 17:57:05 +0500

    methods

--- Infrustructure/Service/NpgsqlService.cs
+++ Infrustructure/Service/NpgsqlService.cs
@@ -1,2 +1,18 @@
 namespace Service;
+
+using System.Security.Cryptography.X509Certificates;
 using Npgsql;
+
+
+
+public static class NpgsqlService
+{
+    public static void CreateDatabase(string databaseName)
+    {
+        
+    }
+    public static void CreateTable(string databaseName,string tablename,string[] collumns)
+    {
+
+    }
+}
